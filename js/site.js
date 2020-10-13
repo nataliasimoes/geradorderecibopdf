634bc3c13ca86ccc8c0e16b7390ad0e5613a6f21
Natália Simões <fnatalias@hotmail.com>
2020-10-13 19:39:07 -0300

    Subindo arquivos

---
+++ js/site.js
@@ -0,0 +1,141 @@
+// Uma função que é chamada quando o DOM é caregado
+function chama() {
+  var nomeBanco = document.getElementById('nomeBanco');
+  var numeRecibo = document.getElementById('numeroRecibo');
+  var nomeBene = document.getElementById('nomeBeneficiario');
+  var razao = document.getElementById('razao');    
+  var cpfBene = document.getElementById('cpfBeneficiario');
+  var pagador = document.getElementById('pagador');
+  var cpfPagador = document.getElementById('cpfPagador');
+  var datV = document.getElementById('dataVencimento');
+  var datP = document.getElementById('dataPagamento');
+  var valor = document.getElementById('valor');
+  var desconto = document.getElementById('desconto');
+  var abatimento= document.getElementById('abatimento');
+  var bonificacao = document.getElementById('bonificacao');
+  var multa = document.getElementById('multa');
+  var juros = document.getElementById('juros');
+  var valorTotal = document.getElementById('valorTotal');
+  var descricao = document.getElementById('descricao');
+  
+  if(nomeBanco.value != ""){
+    if(numeRecibo.value != ""){
+      if(nomeBene.value != ""){
+        if(cpfBene.value != ""){
+          if(pagador.value != ""){
+            if(cpfPagador.value != ""){
+              if(datV.value != ""){
+                if(datP.value != ""){
+                  if(valor.value != ""){
+                    if(desconto.value != ""){
+                      if(abatimento.value != ""){
+                        if(bonificacao.value != ""){
+                          if(multa.value != ""){
+                            if(juros.value != ""){
+                              if(valorTotal.value != ""){
+                                pega();
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
+}
+
+// Função que pega o valor dos inputs
+function pega() {
+  var nomeBanco = document.getElementById('nomeBanco').value;
+  var numeRecibo = document.getElementById('numeroRecibo').value;
+  var nomeBene = document.getElementById('nomeBeneficiario').value;
+  var razao = document.getElementById('razao').value;
+  var cpfBene = document.getElementById('cpfBeneficiario').value;
+  var pagador = document.getElementById('pagador').value;
+  var cpfPagador = document.getElementById('cpfPagador').value;
+  var datV = document.getElementById('dataVencimento').value;
+  var datP = document.getElementById('dataPagamento').value;
+  var valor = document.getElementById('valor').value;
+  var desconto = document.getElementById('desconto').value;
+  var abatimento= document.getElementById('abatimento').value;
+  var bonificacao = document.getElementById('bonificacao').value;
+  var multa = document.getElementById('multa').value;
+  var juros = document.getElementById('juros').value;
+  var valorTotal = document.getElementById('valorTotal').value;
+  var descricao = document.getElementById('descricao').value;
+  
+  
+  // Função que cria o PDF
+  var pdf = new jsPDF();
+  
+  pdf.setProperties({
+    title: 'Gerador de Recibos',
+    subject: 'Recibo ' + numeRecibo,
+    author: '-------------',
+    keywords: 'gerador de recibos',
+    creator: 'gPDF, javascript, web 2.0, ajax'
+  });
+  
+  
+  pdf.setFontSize(12);
+  pdf.text(75, 20, "Comprovante de Pagamento");
+  pdf.text(85, 25, "Boleto de Cobrança");
+  
+  
+  pdf.setFontSize(18); //Tamanho da fonte
+  pdf.setTextColor(150); // cor da fonte cinza claro
+  pdf.text(92, 40, "n° "+numeRecibo);
+  
+  pdf.setTextColor(0); // cor da fonte preto
+  pdf.setFont("helvetica");
+  pdf.setFontSize(10);
+  pdf.text(35, 55, "Beneficiário: "+nomeBene);
+  pdf.text(35, 60, "CPF/CNPJ: "+cpfBene);
+  pdf.text(35, 65, "Razão social: "+razao)
+  pdf.text(35, 70, 'Banco: '+nomeBanco);
+  pdf.text(35, 75, "________________________________________________________________________");
+  
+  pdf.text(35, 80, 'Pagador: '+pagador);
+  pdf.text(35, 85, "CPF/CNPJ: "+cpfPagador);
+  
+  
+  pdf.text(35, 90, "________________________________________________________________________");
+  
+  pdf.text(35, 95, 'Data de emissão: '+datP);
+  pdf.text(110, 95, 'Data de validade: '+datV);
+  pdf.text(35, 100, "________________________________________________________________________");
+  
+  pdf.text(35, 105, "Valor:"+valor+ "R$");
+  pdf.text(110, 105, "Desconto:"+desconto+ "R$");
+  pdf.text(35, 110, "Abatimento:"+abatimento+ "R$");
+  pdf.text(110, 110, "Bonificação:"+bonificacao+ "R$");
+  pdf.text(35, 115, "________________________________________________________________________");
+  
+  pdf.setFontSize(12);
+  pdf.text(87, 125, "Valor total: "+valorTotal+"R$");
+  
+  pdf.setFontSize(10);
+  pdf.text(35, 135, "________________________________________________________________________");
+  
+  pdf.text(35, 140, "Descrição: "+descricao );
+  
+  pdf.setFontSize(8);
+  pdf.text(75, 145, "Banco Central do Brasil estabelece o prazo de");
+  pdf.text(70, 150, "3 dias úteis para compensar o boleto de pagamento.");
+  
+  
+  // pdf.save('recibo_'+numeRecibo+'_'+pagador+'.pdf');
+
+  // pdf.output('dataurlnewwindow', {});
+
+  window.open(pdf.output('bloburl')) 
+  
+}
